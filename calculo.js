7d520d188084327f6da0c1ecf08e677d856cd9c5
Diez111 <71948453+Diez111@users.noreply.github.com>
2022-05-25 01:50:56 -0300

    Add files via upload

---
+++ calculo.js
@@ -0,0 +1,63 @@
+function COM() {
+                var n1 = document.getElementById('txtN1').value;
+
+
+                var s = document.getElementById('unidades');
+            var valor1 = s.options[s.selectedIndex].value;
+
+            var a = document.getElementById('unidades2');
+        var valor2 = a.options[a.selectedIndex].value;
+
+
+           /*PARTE KGF/CM2*/
+          if (valor1 == '1' && valor2 == '4') {
+            var multi = parseInt(n1) * 1
+            alert("El pasaje es: "+multi)
+          }
+
+          else if (valor1 == '1' && valor2 == '5') {
+            var multi = parseInt(n1) * 10.0003
+            alert("El pasaje es: "+multi)
+          }
+
+          else if (valor1 == '1' && valor2 == '6') {
+            var multi = parseInt(n1) * 0.980665
+            alert("El pasaje es: "+multi)
+          }
+
+          /*PARTE mH2o*/
+
+          else if (valor1 == '2' && valor2 == '4') {
+            var multi = parseInt(n1) * 0.099997
+            alert("El pasaje es: "+multi)
+          }
+
+          else if (valor1 == '2' && valor2 == '5') {
+            var multi = parseInt(n1) * 1
+            alert("El pasaje es: "+multi)
+          }
+
+          else if (valor1 == '2' && valor2 == '6') {
+            var multi = parseInt(n1) * 0.098064
+            alert("El pasaje es: "+multi)
+          }
+
+            /*PARTE Bar*/
+
+            else if (valor1 == '3' && valor2 == '4') {
+              var multi = parseInt(n1) * 1.0197
+              alert("El pasaje es: "+multi)
+            }
+
+            else if (valor1 == '3' && valor2 == '5') {
+              var multi = parseInt(n1) * 10.1974
+              alert("El pasaje es: "+multi)
+            }
+
+            else if (valor1 == '3' && valor2 == '6') {
+              var multi = parseInt(n1) * 1
+              alert("El pasaje es: "+multi)
+            }
+
+
+    }
